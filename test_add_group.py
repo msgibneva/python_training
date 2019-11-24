8a4f99ff46d97012892e8bcd1962a0bba7ccf8bc
Mari Sgibneva <mari.sgibnev@gmail.com>
2019-11-24 21:00:52 +0300

    Создано окружение env Добавлены библиотеки selenium и pytest

--- test_add_group.py
+++ test_add_group.py
@@ -45,20 +45,8 @@ class TestAddGroup(unittest.TestCase):
         except NoAlertPresentException as e: return False
         return True
     
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
     def tearDown(self):
         self.wd.quit()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
