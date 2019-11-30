21cf2363e1c5e7f48ca95ce3a46b3488b6744e1a
Mari Sgibneva <mari.sgibnev@gmail.com>
2019-11-30 22:07:41 +0300

    Выделен класс "Subscriber"

---
+++ subscriber.py
@@ -0,0 +1,29 @@
+class Subscriber:
+
+    def __init__(self, name, secondname, surname, nick, title, companyname, homeaddress, homephone,
+                              mobilephone, work, faxnumber, email1, email2, email3, homesite, bday, bmonth, aday,
+                              amonth, byear, ayear, addresssec, sechomenumber, notes):
+        self.name = name
+        self.secondname = secondname
+        self.surname = surname
+        self.nick = nick
+        self.title = title
+        self.companyname = companyname
+        self.homeaddress = homeaddress
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.work = work
+        self.faxnumber = faxnumber
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homesite = homesite
+        self.bday = bday
+        self.bmonth = bmonth
+        self.aday = aday
+        self.amonth = amonth
+        self.byear = byear
+        self.ayear = ayear
+        self.addresssec = addresssec
+        self.sechomenumber = sechomenumber
+        self.notes = notes
--- test_add_sub.py
+++ test_add_sub.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import unittest
+from subscriber import Subscriber
 
 class TestAddSub(unittest.TestCase):
     def setUp(self):
@@ -12,67 +13,75 @@ class TestAddSub(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username = "admin", password = "secret")
-        self.create_new_subscriber(wd, name = "123", secondname = "dfgtg", surname="auidhls", nick="nic", title="title", companyname="company", homeaddress="address", homephone="homephone",
+        self.create_new_subscriber(wd, Subscriber(name = "123", secondname = "dfgtg", surname="auidhls", nick="nic", title="title", companyname="company", homeaddress="address", homephone="homephone",
                                    mobilephone="mobile", work="work", faxnumber="fax", email1="email1", email2="email2", email3="email3", homesite="homesite", bday="12", bmonth="December",
-                                   aday="17", amonth="November", byear="1995", ayear="2200", addresssec="addresssec", sechomenumber="sechome", notes="notes")
+                                   aday="17", amonth="November", byear="1995", ayear="2200", addresssec="addresssec", sechomenumber="sechome", notes="notes"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
-    def create_new_subscriber(self, wd, name, secondname, surname, nick, title, companyname, homeaddress, homephone,
-                              mobilephone, work, faxnumber, email1, email2, email3, homesite, bday, bmonth, aday,
-                              amonth, byear, ayear, addresssec, sechomenumber, notes):
+    def test_add_empty_sub(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username = "admin", password = "secret")
+        self.create_new_subscriber(wd, Subscriber(name = "", secondname = "", surname="", nick="", title="", companyname="", homeaddress="", homephone="",
+                                   mobilephone="", work="", faxnumber="", email1="", email2="", email3="", homesite="", bday="-", bmonth="-",
+                                   aday="-", amonth="-", byear="", ayear="", addresssec="", sechomenumber="", notes=""))
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def create_new_subscriber(self, wd, subscriber):
         # init subscriber creation
         wd.find_element_by_link_text("add new").click()
         # fill profile
         wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys(name)
+        wd.find_element_by_name("firstname").send_keys(subscriber.name)
         wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").send_keys(secondname)
+        wd.find_element_by_name("middlename").send_keys(subscriber.secondname)
         wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").send_keys(surname)
+        wd.find_element_by_name("lastname").send_keys(subscriber.surname)
         wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").send_keys(nick)
+        wd.find_element_by_name("nickname").send_keys(subscriber.nick)
         wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(subscriber.title)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").send_keys(companyname)
+        wd.find_element_by_name("company").send_keys(subscriber.companyname)
         wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").send_keys(homeaddress)
+        wd.find_element_by_name("address").send_keys(subscriber.homeaddress)
         wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").send_keys(homephone)
+        wd.find_element_by_name("home").send_keys(subscriber.homephone)
         wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").send_keys(mobilephone)
+        wd.find_element_by_name("mobile").send_keys(subscriber.mobilephone)
         wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").send_keys(work)
+        wd.find_element_by_name("work").send_keys(subscriber.work)
         wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").send_keys(faxnumber)
+        wd.find_element_by_name("fax").send_keys(subscriber.faxnumber)
         wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").send_keys(email1)
+        wd.find_element_by_name("email").send_keys(subscriber.email1)
         wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").send_keys(email2)
+        wd.find_element_by_name("email2").send_keys(subscriber.email2)
         wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").send_keys(email3)
+        wd.find_element_by_name("email3").send_keys(subscriber.email3)
         wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").send_keys(homesite)
+        wd.find_element_by_name("homepage").send_keys(subscriber.homesite)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(bday)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(subscriber.bday)
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(bmonth)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(subscriber.bmonth)
         wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").send_keys(byear)
+        wd.find_element_by_name("byear").send_keys(subscriber.byear)
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(aday)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(subscriber.aday)
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(amonth)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(subscriber.amonth)
         wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").send_keys(ayear)
+        wd.find_element_by_name("ayear").send_keys(subscriber.ayear)
         wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").send_keys(addresssec)
+        wd.find_element_by_name("address2").send_keys(subscriber.addresssec)
         wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").send_keys(sechomenumber)
+        wd.find_element_by_name("phone2").send_keys(subscriber.sechomenumber)
         wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").send_keys(notes)
+        wd.find_element_by_name("notes").send_keys(subscriber.notes)
         # submit new subscriber
         wd.find_element_by_name("submit").click()
 
