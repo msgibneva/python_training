bb419797d190eb095892c27af6a0a142245f9c8c
Mari Sgibneva <mari.sgibnev@gmail.com>
2019-12-01 14:05:37 +0300

    Созданы пакеты для разных зон ответственности, выделен класс для управления сессиями

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,11 +11,11 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="nameofgroup", header="groupheader", footer="comment"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
