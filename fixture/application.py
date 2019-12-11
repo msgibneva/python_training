43fff504f574686c4ad9dcade0b951b3414540bb
Mari Sgibneva <mari.sgibnev@gmail.com>
2019-12-11 19:42:00 +0300

    Исправлена опечатка

--- fixture/application.py
+++ fixture/application.py
@@ -21,7 +21,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_name("add"))) > 0:
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("add"))) > 0:
             wd.get("http://localhost/addressbook/")
 
     def destroy(self):
