7c2cb377695bfddc0aeaa1a99bee4362b920c075
Mari Sgibneva <mari.sgibnev@gmail.com>
2019-12-19 21:19:33 +0300

    Добавлено сравнение списка групп до/после удаления, добавления и модификации групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,9 +5,15 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="grname"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="new"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="grname"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="new"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
