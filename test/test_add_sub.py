9665dcd8c9234a363d57b2ad594e006c95c9d32c
Mari Sgibneva <mari.sgibnev@gmail.com>
2020-01-01 20:48:17 +0300

    Тест создания контактов дополнен для использования случайных данных

--- test/test_add_sub.py
+++ test/test_add_sub.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.subscriber import Subscriber
+import pytest
+import random
+import string
 
+def random_string (prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_sub(app):
+testdata = [Subscriber(firstname ="", secondname ="", lastname="", nick="", title="", companyname="", homeaddress="", homephone="",
+                                     mobilephone="", work="", faxnumber="", email1="", email2="", email3="", homesite="", addresssec="", sechomenumber="", notes="")] + [
+    Subscriber(firstname=random_string("firstname", 10), secondname=random_string("secondname", 20), lastname=random_string("lastname", 20))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("subscriber", testdata, ids=[repr(x) for x in testdata])
+def test_add_sub(app, subscriber):
     old_sub = app.subscriber.get_sub_list()
-    subscriber = Subscriber(firstname ="123", secondname ="dfgtg", lastname="auidhls", nick="nic", title="title", companyname="company", homeaddress="address", homephone="homephone",
-                                     mobilephone="mobile", work="work", faxnumber="fax", email1="email1", email2="email2", email3="email3", homesite="homesite", addresssec="addresssec", sechomenumber="sechome", notes="notes")
     app.subscriber.create(subscriber)
     assert len(old_sub) + 1 == app.subscriber.count()
     new_sub = app.subscriber.get_sub_list()
