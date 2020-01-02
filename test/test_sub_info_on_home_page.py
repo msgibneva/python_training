2f0cd483577e10b800d0b132ba1dd544ee152a89
Mari Sgibneva <mari.sgibnev@gmail.com>
2020-01-02 20:50:53 +0300

    Проверки информации об абоненте объединены в один тест

--- test/test_sub_info_on_home_page.py
+++ test/test_sub_info_on_home_page.py
@@ -1,21 +1,17 @@
-import re
 from random import randrange
+import re
 
 
-def test_phones_on_home_page(app):
+def test_sub_info(app):
     old_sub = app.subscriber.get_sub_list()
     index = randrange(len(old_sub))
     subscriber_from_home_page = app.subscriber.get_sub_list()[index]
     subscriber_from_edit_page = app.subscriber.get_info_from_edit_page(index)
+    assert subscriber_from_home_page.homeaddress == subscriber_from_edit_page.homeaddress
+    assert subscriber_from_home_page.firstname == subscriber_from_edit_page.firstname
+    assert subscriber_from_home_page.lastname == subscriber_from_edit_page.lastname
     assert subscriber_from_home_page.all_phones_frome_home_page == merge_phones_like_on_home_page(subscriber_from_edit_page)
-
-def test_phones_on_sub_view_page(app):
-    subscriber_from_view_page = app.subscriber.get_sub_from_view_page(0)
-    subscriber_from_edit_page = app.subscriber.get_info_from_edit_page(0)
-    assert subscriber_from_view_page.homephone == subscriber_from_edit_page.homephone
-    assert subscriber_from_view_page.mobilephone == subscriber_from_edit_page.mobilephone
-    assert subscriber_from_view_page.work == subscriber_from_edit_page.work
-    assert subscriber_from_view_page.sechomenumber == subscriber_from_edit_page.sechomenumber
+    assert subscriber_from_home_page.all_emails_frome_home_page == merge_emails_like_on_home_page(subscriber_from_edit_page)
 
 def clear(s):
     return re.sub("[() -]", "", s)
@@ -25,4 +21,10 @@ def merge_phones_like_on_home_page(subscriber):
                              map(lambda x: clear(x),
                                  filter(lambda x: x is not None,
                                          [subscriber.homephone, subscriber.mobilephone, subscriber.work, subscriber.sechomenumber]))))
+
+def merge_emails_like_on_home_page(subscriber):
+    return "\n".join(filter(lambda x: x != "",
+                             map(lambda x: clear(x),
+                                 filter( lambda x: x is not None,
+                                         [subscriber.email1, subscriber.email2, subscriber.email3]))))
 
