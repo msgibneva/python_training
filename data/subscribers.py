cda713b1f60ef8f4d1962a438eb4f875aeaf564b
Mari Sgibneva <mari.sgibnev@gmail.com>
2020-01-12 21:42:18 +0300

    Реализована динамическая генерация тестов

---
+++ data/subscribers.py
@@ -0,0 +1,8 @@
+from model.subscriber import Subscriber
+
+
+testdata = [
+    Subscriber(firstname="firstname1", secondname="secondname1", lastname="lastname1"),
+    Subscriber(firstname="firstname2", secondname="secondname2", lastname="lastname2"),
+]
+
--- test/test_add_sub.py
+++ test/test_add_sub.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.subscriber import Subscriber
-import pytest
-from data.add_sub import constant as testdata
 
 
-@pytest.mark.parametrize("subscriber", testdata, ids=[repr(x) for x in testdata])
-def test_add_sub(app, subscriber):
+def test_add_sub(app, data_subscribers):
+    subscriber = data_subscribers
     old_sub = app.subscriber.get_sub_list()
     app.subscriber.create(subscriber)
     assert len(old_sub) + 1 == app.subscriber.count()
