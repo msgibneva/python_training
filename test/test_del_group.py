a0bdf76dfb1cf97e1fd05c2916bdd7afaea587f6
Mari Sgibneva <mari.sgibnev@gmail.com>
2020-01-15 22:39:28 +0300

    Переделаны тесты для удаления и добавления групп с использованием БД

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,14 +1,15 @@
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_delete_some_group(app):
-    old_groups = app.group.get_group_list()
-    if app.group.count() == 0:
+def test_delete_some_group(app, db):
+    old_groups = db.get_group_list()
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="grname"))
-    index = randrange(len(old_groups))
-    app.group.delete_group_by_index(index)
+    #определяем группу для удаления
+    group = random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
     assert len(old_groups) - 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index:index+1] = []
+    new_groups = db.get_group_list()
+    old_groups.remove(group)
     assert old_groups == new_groups
